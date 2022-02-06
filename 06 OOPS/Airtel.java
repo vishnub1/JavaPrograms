b4fbf5ed721cb923831991d8d7805f2e83eeedc4
Vishnu Biradar <91277493+vishnub1@users.noreply.github.com>
2022-02-06 21:07:26 +0530

    Delete 06 OOPS directory

--- 06 OOPS/Airtel.java
+++
@@ -1,7 +0,0 @@
-class Airtel implements SimMainFile {
-	public void call(){
-		System.out.println("this is calla for Airtel");
-	public void sms(){
-		System.out.println("SMS from Airtel !");
-	}
-}
