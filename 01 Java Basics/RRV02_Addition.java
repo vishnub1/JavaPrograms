00e8f1fafedc85697f298c9990eb2564b5127532
Vishnu Biradar <91277493+vishnub1@users.noreply.github.com>
2022-02-06 21:12:52 +0530

    Add files via upload

---
+++ 01 Java Basics/RRV02_Addition.java
@@ -0,0 +1,9 @@
+class RRV02_Addition {
+	public static void main(String[] args) {
+		int a = Integer.parseInt(args[0]);
+		int b = Integer.parseInt(args[1]);
+		int c = a + b;
+
+		System.out.println("Result :" + c);
+	}
+}
