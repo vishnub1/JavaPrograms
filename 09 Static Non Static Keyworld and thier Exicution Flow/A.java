9a0c31e6dcdee690f99bbfb320612230ae334d1a
Vishnu Biradar <91277493+vishnub1@users.noreply.github.com>
2022-02-21 20:53:37 +0530

    Create A.java

---
+++ 09 Static Non Static Keyworld and thier Exicution Flow/A.java
@@ -0,0 +1,24 @@
+//modularity
+// modularity means dividing a big task into a small pieces
+// in java we can achieve modularity.
+//methods and class with classes.
+
+class A {
+	static void add(int a,int b){
+		//addition
+		System.out.println(a+b);
+	}
+	static void sub(int a,int b){
+		//Substraction
+		System.out.println(a-b);
+	}
+	static void mul(int a ,int b){
+		//multiplication
+		System.out.println(a*b);
+	}
+	static void div(int a, int b){
+		//div
+		System.out.println(a/b);
+	}
+
+}
